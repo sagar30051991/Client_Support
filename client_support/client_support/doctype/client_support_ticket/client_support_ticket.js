e4339f3d6f492f27bb3026de28bece47b6e12f02
Makarand <mbauskar@gmail.com>
2015-04-01 12:28:05 +0530

    Added user permission & resolution field

--- client_support/client_support/doctype/client_support_ticket/client_support_ticket.js
+++ client_support/client_support/doctype/client_support_ticket/client_support_ticket.js
@@ -1,4 +1,32 @@
 cur_frm.cscript.onload = function(){
-	console.log(cur_frm.doc)
-	this.frm.doc.opening_date = get_today()
+	/*this.frm.doc.opening_date = get_today()*/
+	//this.frm.doc.opening_date = frappe.datetime.get_today()
+	//console.log(cur_frm.doc)
+	/*if('Client' == user_roles[0]){
+		this.frm.set_df_property("resolution_details","read_only",1);
+	}
+	else if('Support User' == user_roles[0]){
+		this.frm.set_df_property("issue","read_only",1);
+		this.frm.set_df_property("project","read_only",1)
+		this.frm.set_df_property("description","read_only",1)
+	}*/
+
+	if(isClient(user_roles))
+		this.frm.set_df_property("resolution_details","read_only",1);
+	else{
+		this.frm.set_df_property("issue","read_only",1);
+		this.frm.set_df_property("project","read_only",1);
+		this.frm.set_df_property("description","read_only",1);
+	}
+}
+
+isClient = function(role_list){
+	// return true if user role Client
+	console.log("inClient");
+	for(role in user_roles){
+		if(role == 'Client')
+			return true;
+		else if(role == 'Support User')
+			return false;
+	}
 }
--- client_support/client_support/doctype/client_support_ticket/client_support_ticket.py
+++ client_support/client_support/doctype/client_support_ticket/client_support_ticket.py
@@ -4,6 +4,30 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+from frappe.utils import now_datetime, now
+from frappe import msgprint
+import datetime
+import time
 
 class ClientSupportTicket(Document):
-	pass
+	
+	def autoname(self):
+		frappe.errprint("autoname")
+		
+		project_name = self.project[:3]
+		self.name = make_autoname(project_name + ".####")
+
+	def validate(self):
+		frappe.errprint("validate")
+		
+		if not self.project:
+			msgprint("Please select the project")
+
+	def on_update(self):
+		# if status is closed then set closing date
+		frappe.errprint("on_update")
+		frappe.errprint(now_datetime())
+
+		if self.status == "Close":
+			self.closing_date = datetime.datetime.strptime(now(),'%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d %H:%M:%S')
