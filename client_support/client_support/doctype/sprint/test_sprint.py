2b2ba9ab8dbd96db2323ef9c7e74e85de4bec9cd
pranalik <pranali.k@indictranstech.com>
2015-04-14 13:19:03 +0200

    changed project

---
+++ client_support/client_support/doctype/sprint/test_sprint.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, Makarand Bauskar and Contributors
+# See license.txt
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Sprint')
+
+class TestSprint(unittest.TestCase):
+	pass
